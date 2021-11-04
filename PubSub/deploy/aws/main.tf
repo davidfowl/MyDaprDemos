870d9c654fcfcb4913e09546d683af478075d5ad
Donovan Brown <dlb@dlbonline.com>
2021-11-04 14:03:07 +0000

    AWS Terraform fixed for PubSub

--- PubSub/deploy/aws/main.tf
+++ PubSub/deploy/aws/main.tf
@@ -6,10 +6,22 @@ provider "aws" {
 
 resource "aws_sqs_queue" "aws_queue" {
   name = "app1"
+  tags = {
+    "dapr-queue-name" = "app1"
+  }
 }
 
 resource "aws_sns_topic" "aws_notification" {
   name = "neworder"
+  tags = {
+    "dapr-topic-name" = "neworder"
+  }
+}
+
+resource "aws_sns_topic_subscription" "neworder_sqs_target" {
+  topic_arn = aws_sns_topic.aws_notification.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.aws_queue.arn
 }
 
 resource "aws_dynamodb_table" "dapr_state_store" {
