0863e52582101b3052acefd2834fb8d271e82a4a
paulyuk <paulyuk@microsoft.com>
2021-11-14 11:17:04 -0800

    HTTP proxy - partial

--- DevOps/src/provider/Program.cs
+++ DevOps/src/provider/Program.cs
@@ -2,23 +2,33 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddDapr();
+builder.Services.AddHttpClient();
 
 // Configure and enable middlewares
 var app = builder.Build();
+
+var baseURL = Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost" + ":" + Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500"; //reconfigure cpde to make requests to Dapr sidecar
+app.Logger.LogInformation("Init: base URL set: " + baseURL);
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapPost("/tweets", async (Tweet t, Dapr.Client.DaprClient client) =>
+app.MapPost("/tweets", async (Tweet t, Dapr.Client.DaprClient daprClient, IHttpClientFactory httpClientFactory) =>
  {
-     app.Logger.LogInformation("/tweets invoked...");
-     var scoredTweet = await client.InvokeMethodAsync<Tweet, AnalyzedTweet>("processor", "score", t);
+     app.Logger.LogInformation("/tweets service invoked...");
+     var httpClient = httpClientFactory.CreateClient();
+     httpClient.DefaultRequestHeaders.Add("dapr-app-id", "processor"); //only code needed for Dapr service discovery - add a header to enable HTTP Proxy
+     var response = await httpClient.PostAsJsonAsync<Tweet>(baseURL + "/score", t);
+     AnalyzedTweet scoredTweet = await response.Content.ReadFromJsonAsync<AnalyzedTweet>();
+     //var scoredTweet = await daprClient.InvokeMethodAsync<Tweet, AnalyzedTweet>("processor", "score", t); //optionally use Dapr SDK for Service Invoke
+
      app.Logger.LogInformation("/tweet scored, saving to state store");
-     await client.SaveStateAsync<AnalyzedTweet>("statestore", t.Id, scoredTweet);
-     app.Logger.LogInformation("/tweet saved, posting to pubsub");
-     await client.PublishEventAsync<AnalyzedTweet>("pubsub", "scored", scoredTweet);
+     await daprClient.SaveStateAsync<AnalyzedTweet>("statestore", t.Id, scoredTweet);
+        
+     app.Logger.LogInformation("/tweet saved, publishing to pubsub");
+     await daprClient.PublishEventAsync<AnalyzedTweet>("pubsub", "scored", scoredTweet);
      app.Logger.LogInformation("/tweet processed");
  });
 
